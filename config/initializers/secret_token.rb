0bb91ff0eeed22655a8768e8a0616868f10493d6
Rushaine McBean <codekid24@gmail.com>
2013-11-21 01:14:00 -0500

    Initial app creation commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Knightrider::Application.config.secret_key_base = '371eab974c448d66722265712ba8af238f177a034bfd645f0d2ec0bc03dd1dfbe67386d7495d4c3af24050a61399d0f4ef22d17fa33ef7651a0a726467bdb1fc'
