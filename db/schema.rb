8764f9b5a90bde0d06720d29a8f939bf8834e122
Rushaine McBean <codekid24@gmail.com>
2013-11-27 13:56:23 -0500

    Create a robot model with the name & description fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131125040736) do
+ActiveRecord::Schema.define(version: 20131127182330) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  create_table "robots", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
