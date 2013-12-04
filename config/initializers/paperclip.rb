ab58f800b78ab5da89a52e62c41c0e71e81f3363
Rushaine McBean <codekid24@gmail.com>
2013-12-04 15:15:00 -0500

    Add in s3 config vars for heroku

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,14 @@
+if ENV["S3_ACCESS_KEY" ]
+  Paperclip::Attachment.default_options[:storage] = :s3
+  Paperclip::Attachment.default_options[:s3_credentials] = {
+    access_key_id: ENV["S3_ACCESS_KEY"],
+    secret_access_key: ENV["S3_SECRET_KEY"]
+  }
+  Paperclip::Attachment.default_options[:bucket] = ENV["S3_UPLOAD_BUCKET"]
+  Paperclip::Attachment.default_options[:s3_host_alias] = ENV["S3_UPLOAD_BUCKET"]
+  Paperclip::Attachment.default_options[:url] = ":s3_alias_url"
+  Paperclip::Attachment.default_options[:s3_protocol] = ""
+  Paperclip::Attachment.default_options[:path] = ":class/:attachment/#{Rails.env}/:id/:style/:basename.:extension"
+end
+
+
